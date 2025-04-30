4338b6df1318c0c39f5b5afd244a129176fefe9d
surendra kumawat <jivankumawat7742@gmail.com>
2025-04-30 17:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (39.68%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,6 +10,9 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || head.next == null){
+            return head;
+        }
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode prev = dummy;
