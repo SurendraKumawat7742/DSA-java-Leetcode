ea95cfb27f9c54b4e0bd51e1a175e63efc016d08
surendra kumawat <jivankumawat7742@gmail.com>
2025-05-19 14:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (60.66%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -4,24 +4,10 @@ class Solution {
         int a = nums[0];
         int b = nums[1];
         int c = nums[2];
-        boolean isTrue = false;
         if((a+b > c) && (b+c) > a && (a+c) > b){
-            isTrue = true;
-        } 
-        if(isTrue){
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<n; i++){
-            if(!set.contains(nums[i])){
-                set.add(nums[i]);
-            }
-        }
-        if(set.size() == 1){
-            return  "equilateral";
-        }else if(set.size() == 2){
-             return  "isosceles";
-        }else{
-             return  "scalene";
-        }
+          if(a==b && b==c) return "equilateral";
+          else if(a==b || b==c || a==c) return "isosceles" ;
+          else return "scalene";
         }else{
             return "none";
         }
