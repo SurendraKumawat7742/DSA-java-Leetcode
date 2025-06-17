aae01435100d012eedfdc51ab7445ea726b438bb
surendra kumawat <jivankumawat7742@gmail.com>
2025-06-17 21:33:52 +0530

    Time: 23 ms (94.50%) | Memory: 56.7 MB (66.25%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,32 +1,37 @@
 class Solution {
-    public boolean pri(int[] nums,int n,int value){
-        if(n<nums.length) return false;
-        for(int i=0;i<nums.length;i++){
-            int x = nums[i];
-            // while(x>0){
-            //     x-=value;
-            //     n--;
-            // }
-            int temp = x/value;
-            if(x%value != 0) temp++;
-            n-=temp;
-            if(n<0) return false;
-        }
-        return true;
-    } 
     public int minimizedMaximum(int n, int[] quantities) {
-        int l = 1;
-        // int r = Integer.MAX_VALUE;
-        int r = (int)Math.pow(10,5);
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(pri(quantities,n,mid)){
-                r = mid-1;
+        int x= quantities.length;
+       // Arrays.sort(quantities);
+        int low = 1;
+        int high = 0;
+        for(int lol : quantities)
+        {
+            high = Math.max(high,lol);
+        }
+        int ans = 0;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+
+            if(ifPossible(n,quantities,mid))
+            {
+                ans = mid;
+                high = mid -1;
             }
             else{
-                l = mid+1;
+                low = mid +1;
             }
         }
-        return l;
+       return ans; 
+    }
+    private boolean ifPossible(int n , int quantities[],int ans)
+    {
+        int count = 0;
+
+        for(int i = 0 ; i < quantities.length;i++)
+        {
+         count += (quantities[i] + ans - 1) / ans;
+        } 
+       return count<=n;
     }
 }
