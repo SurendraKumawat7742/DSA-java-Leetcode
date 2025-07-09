13b93f7281cff253e9364b3150aeb95e6fa98bec
surendra kumawat <jivankumawat7742@gmail.com>
2025-07-09 17:03:27 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (98.62%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,24 +1,50 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
+        // int n = grid.length;
+        // int m = grid[0].length;
+        // int[] res = new int[2];
+        // int sum = 0;
+        // int total = (n*m)*((n*m)+1)/2;
+        // int repeat = 0;
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(!set.contains(grid[i][j])){
+        //             set.add(grid[i][j]);
+        //             sum += grid[i][j];
+        //         }else{
+        //             repeat = grid[i][j];
+        //         }
+        //     }
+        // }
+        // res[1] = total-sum;
+        // res[0] = repeat;
+        // return res;
+
+
+    //2nd
         int n = grid.length;
-        int m = grid[0].length;
-        int[] res = new int[2];
-        int sum = 0;
-        int total = (n*m)*((n*m)+1)/2;
-        int repeat = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(!set.contains(grid[i][j])){
-                    set.add(grid[i][j]);
-                    sum += grid[i][j];
-                }else{
-                    repeat = grid[i][j];
-                }
+        int size = n * n;
+        int[] count = new int[size + 1]; // index 1 to n^2
+
+        // Count frequency of each number in the grid
+        for (int[] row : grid) {
+            for (int num : row) {
+                count[num]++;
             }
         }
-        res[1] = total-sum;
-        res[0] = repeat;
-        return res;
+
+        int repeated = -1, missing = -1;
+
+        // Find the repeated and missing numbers
+        for (int i = 1; i <= size; i++) {
+            if (count[i] == 2) {
+                repeated = i;
+            } else if (count[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{repeated, missing};
     }
 }
