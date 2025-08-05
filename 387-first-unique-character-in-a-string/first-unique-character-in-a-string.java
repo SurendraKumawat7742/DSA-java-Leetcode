323e688abfc4da1b95b7fd66cb8bd67b7f8cd1ad
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-05 19:00:03 +0530

    Time: 6 ms (80.76%) | Memory: 45.1 MB (83.25%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,19 +1,42 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> mp = new HashMap<>();
-        int n = s.length();
-        for(int i=0; i<n; i++){
-            char c = s.charAt(i);
-            mp.put(c, mp.getOrDefault(c,0)+1);
-        }
-        int idx = -1;
-        for(int i=0; i<n; i++){
-            char c = s.charAt(i);
-            if(mp.get(c) == 1){
-                idx = i;
-                break;
+//1st
+        // Map<Character, Integer> mp = new HashMap<>();
+        // int n = s.length();
+        // for(int i=0; i<n; i++){
+        //     char c = s.charAt(i);
+        //     mp.put(c, mp.getOrDefault(c,0)+1);
+        // }
+        // int idx = -1;
+        // for(int i=0; i<n; i++){
+        //     char c = s.charAt(i);
+        //     if(mp.get(c) == 1){
+        //         idx = i;
+        //         break;
+        //     }
+        // }
+        // return idx;
+
+//2nd
+        Queue<Character> q = new LinkedList<>();
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            if (freq[ch - 'a'] == 0) {
+                q.add(ch);
             }
+            freq[ch - 'a']++;
         }
-        return idx;
+        while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+            q.remove();
+        }
+        for (int i = 0;i<s.length();i++) {
+            if(q.isEmpty()) break;
+            if (s.charAt(i) == q.peek()) {
+                return i;
+            } 
+        }
+        
+        return -1;
     }
 }
