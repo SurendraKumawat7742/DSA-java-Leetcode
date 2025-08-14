35e7accb16234c4c298abd4a538e15e87f417f42
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-14 10:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (85.16%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,18 +1,29 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        String[] arr = {"999","888","777","666","555","444","333","222","111","000"};
-        for(int i=0; i<arr.length; i++){
-            int j=0;
-            while(j < num.length()-2){
-                if(arr[i].charAt(0)==num.charAt(j)&&
-                    arr[i].charAt(1)==num.charAt(j+1)&&
-                    arr[i].charAt(2)==num.charAt(j+2)){
-                    return arr[i].toString();
-                }else{
-                    j++;
-                }
-            }
+//1st
+        // String[] arr = {"999","888","777","666","555","444","333","222","111","000"};
+        // for(int i=0; i<arr.length; i++){
+        //     int j=0;
+        //     while(j < num.length()-2){
+        //         if(arr[i].charAt(0)==num.charAt(j)&&
+        //             arr[i].charAt(1)==num.charAt(j+1)&&
+        //             arr[i].charAt(2)==num.charAt(j+2)){
+        //             return arr[i].toString();
+        //         }else{
+        //             j++;
+        //         }
+        //     }
+        // }
+        // return "";
+
+
+//2nd
+        String[] numbers={"999","888","777","666","555","444","333","222","111","000"};
+     for(String i:numbers){
+        if(num.contains(i)){
+            return i;
         }
-        return "";
+     }
+     return "";
     }
 }
