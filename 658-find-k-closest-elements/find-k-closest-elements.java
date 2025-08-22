b3c953055d292e71e630bc99ea138891922fe088
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-22 18:23:07 +0530

    Time: 4 ms (93.23%) | Memory: 45.9 MB (63.48%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,19 +1,39 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> min_heap = new PriorityQueue<>();
-        for (int integer : arr) {
-            if (k > 0) {
-                min_heap.offer(integer);
-                k--;
-            } else if (Math.abs(min_heap.peek() - x) > Math.abs(integer - x)) {
-                min_heap.poll();
-                min_heap.offer(integer);
+
+//1st
+        // PriorityQueue<Integer> min_heap = new PriorityQueue<>();
+        // for (int integer : arr) {
+        //     if (k > 0) {
+        //         min_heap.offer(integer);
+        //         k--;
+        //     } else if (Math.abs(min_heap.peek() - x) > Math.abs(integer - x)) {
+        //         min_heap.poll();
+        //         min_heap.offer(integer);
+        //     }
+        // }
+        // List<Integer> list = new ArrayList<>();
+        // while (!min_heap.isEmpty()) {
+        //     list.add(min_heap.poll());
+        // }
+        // return list;
+
+//2nd
+        int left = 0, right = arr.length - 1;
+
+        // shrink the window until size = k
+        while (right - left + 1 > k) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;  // drop left element
+            } else {
+                right--; // drop right element
             }
         }
-        List<Integer> list = new ArrayList<>();
-        while (!min_heap.isEmpty()) {
-            list.add(min_heap.poll());
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            result.add(arr[i]);
         }
-        return list;
+        return result;
     }
 }
