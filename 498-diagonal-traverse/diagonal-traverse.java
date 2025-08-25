a19d28764920e69e49c51d41ef57655f2b7003b0
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-25 16:30:14 +0530

    Time: 8 ms (21.62%) | Memory: 47.9 MB (10.35%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -33,40 +33,86 @@ class Solution {
         // return result;  
 
 //2nd
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+        // if(n == 0 || m == 0){
+        //     return new int[0];
+        // }
+        // int row = 0;
+        // int col = 0;
+        // int index = 0;
+        // int[] arr = new int[n*m];
+        // boolean up = true;
+        // while(index < n*m){
+        //     arr[index++] = matrix[row][col];
+        //     if(up){
+        //         if(col == m-1){
+        //             row++;
+        //             up = false;
+        //         }else if(row == 0){
+        //             col++;
+        //             up = false;
+        //         }else{
+        //             row--;
+        //             col++;
+        //         }
+        //     }else{
+        //         if(row == n-1){
+        //             col++;
+        //             up = true;
+        //         }else if(col == 0){
+        //             row++;
+        //             up = true;
+        //         }else{
+        //             row++;
+        //             col--;
+        //         }
+        //     }
+        // }
+        // return arr;
+
+//3rd
         int n = matrix.length;
         int m = matrix[0].length;
-        if(n == 0 || m == 0){
-            return new int[0];
-        }
-        int row = 0;
-        int col = 0;
-        int index = 0;
-        int[] arr = new int[n*m];
-        boolean up = true;
-        while(index < n*m){
-            arr[index++] = matrix[row][col];
-            if(up){
-                if(col == m-1){
-                    row++;
-                    up = false;
-                }else if(row == 0){
-                    col++;
-                    up = false;
-                }else{
-                    row--;
-                    col++;
-                }
+        List<List<Integer>> a = new ArrayList<>();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            List<Integer> temp = new ArrayList<>();
+            int j=i, k=0;
+            while(j >= 0 && k < m){
+                temp.add(matrix[j][k]);
+                j--;
+                k++;
+            }
+            if(flag%2 == 0){
+                a.add(temp);
             }else{
-                if(row == n-1){
-                    col++;
-                    up = true;
-                }else if(col == 0){
-                    row++;
-                    up = true;
-                }else{
-                    row++;
-                    col--;
-                }
+                Collections.reverse(temp);
+                a.add(temp);
+            }
+            flag++;
+        }
+        for(int i=1; i<m; i++){
+            List<Integer> temp = new ArrayList<>();
+            int j=n-1, k=i;
+            while(j >= 0 && k < m){
+                temp.add(matrix[j][k]);
+                j--;
+                k++;
+            }
+            if(flag%2 == 0){
+                a.add(temp);
+            }else{
+                Collections.reverse(temp);
+                a.add(temp);
+            }
+            flag++;
+        }
+        int[] arr = new int[n*m];
+        int idx = 0;
+        for(List<Integer> b : a){
+            for(int val : b){
+                arr[idx++] = val;
             }
         }
         return arr;
