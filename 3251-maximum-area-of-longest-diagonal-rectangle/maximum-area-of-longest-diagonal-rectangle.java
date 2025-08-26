637735e13cab2215b90adcb8c4e6e57f87d36112
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-26 10:28:37 +0530

    Time: 1 ms (96.00%) | Memory: 44.9 MB (17.54%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.java
@@ -1,26 +1,45 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] d) {
-        int n = d.length;
-        double[] a1 = new double[n];
-        double[] a2 = new double[n];
-        for(int i=0; i<n; i++){
-            int n1 = d[i][0];
-            int n2 = d[i][1];
-            a1[i] = Math.sqrt(((n1*n1)+(n2*n2)));
-            a2[i] = n1*n2;
-        }
-        double max = a1[0];
-        int idx = 0;
-        for(int i=1; i<n; i++){
-            if(a1[i] > max){
-                max = a1[i];
-                idx = i;
-            }else if(a1[i] == max){
-                if(a2[idx] < a2[i]){
-                    idx = i;
-                }
+//1st
+        // int n = d.length;
+        // double[] a1 = new double[n];
+        // double[] a2 = new double[n];
+        // for(int i=0; i<n; i++){
+        //     int n1 = d[i][0];
+        //     int n2 = d[i][1];
+        //     a1[i] = Math.sqrt(((n1*n1)+(n2*n2)));
+        //     a2[i] = n1*n2;
+        // }
+        // double max = a1[0];
+        // int idx = 0;
+        // for(int i=1; i<n; i++){
+        //     if(a1[i] > max){
+        //         max = a1[i];
+        //         idx = i;
+        //     }else if(a1[i] == max){
+        //         if(a2[idx] < a2[i]){
+        //             idx = i;
+        //         }
+        //     }
+        // }
+        // return (int)a2[idx];
+
+
+//2nd
+        int maxDiaSq = 0;
+        int maxArea = 0;
+        for (int[] dim : d) {
+            int l = dim[0];
+            int w = dim[1];
+            int diaSq = l * l + w * w;
+            int area = l * w;
+            if (diaSq > maxDiaSq) {
+                maxDiaSq = diaSq;
+                maxArea = area;
+            } else if (diaSq == maxDiaSq) {
+                maxArea = Math.max(maxArea, area);
             }
         }
-        return (int)a2[idx];
+        return maxArea;
     }
 }
