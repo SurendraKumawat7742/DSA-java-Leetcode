e521ee3432883b9402bdedf4ccedcb5da20cd4e1
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-28 19:16:56 +0530

    Time: 1531 ms (5.01%) | Memory: 48.3 MB (8.04%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,18 +1,38 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int near = 0;
-        int far = 0;
-        while(far < nums.length-1){
-            int farthest = 0;
-            for(int i=near; i<=far; i++){
-                farthest = Math.max(farthest , i+nums[i]);
-            }
-            near = far+1;
-            far = farthest;
-            if(near > far){
-            return false;
-            }
+    //public boolean canJump(int[] nums) {
+//1st
+        // int near = 0;
+        // int far = 0;
+        // while(far < nums.length-1){
+        //     int farthest = 0;
+        //     for(int i=near; i<=far; i++){
+        //         farthest = Math.max(farthest , i+nums[i]);
+        //     }
+        //     near = far+1;
+        //     far = farthest;
+        //     if(near > far){
+        //     return false;
+        //     }
+        // }
+        // return true;
+    // }
+
+    static Map<Integer, Boolean> memo; //DP
+    public boolean helper(int[] nums, int i){
+        if(i == nums.length-1) return true;
+        if(i >= nums.length || nums[i] == 0) return false;
+        if(memo.containsKey(i)) return false;
+
+        boolean res = false;
+        for(int j=1; j<=nums[i]; j++){
+            res = helper(nums, i+j);
+            if(res) return true;
         }
-        return true;
+        memo.put(i, false);
+        return res;
+    }
+    public boolean canJump(int[] nums){
+        memo = new HashMap<>();
+        return helper(nums, 0);
     }
 }
