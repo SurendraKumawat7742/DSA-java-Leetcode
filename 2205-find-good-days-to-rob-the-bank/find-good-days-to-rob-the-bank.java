319c8a966580fc45951d524b31407b8c734b9f43
surendra kumawat <jivankumawat7742@gmail.com>
2025-08-28 18:22:21 +0530

    Time: 3 ms (100.00%) | Memory: 65.8 MB (5.01%) - LeetSync

--- 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.java
+++ 2205-find-good-days-to-rob-the-bank/find-good-days-to-rob-the-bank.java
@@ -1,32 +1,47 @@
 class Solution {
     public List<Integer> goodDaysToRobBank(int[] security, int time) {
-        List<Integer> ans = new ArrayList<>();
+//1st
+        // List<Integer> ans = new ArrayList<>();
+        // int n = security.length;
+        // int[] prefix = new int[n];
+        // int[] suffix = new int[n];
+        // int cnt = 0;
+        // for (int i = 1; i < n; i++) {
+        //     if (security[i] <= security[i - 1]) {
+        //         cnt++;
+        //         prefix[i] = cnt;
+        //     } else {
+        //         cnt = 0;
+        //     }
+        // }
+        // cnt = 0;
+        // for (int i = n - 2; i >= 0; i--) {
+        //     if (security[i] <= security[i + 1]) {
+        //         cnt++;
+        //         suffix[i] = cnt;
+        //     } else {
+        //         cnt = 0;
+        //     }
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     if (prefix[i] >= time && suffix[i] >= time) {
+        //         ans.add(i);
+        //     }
+        // }
+        // return ans;
+
+
+//2nd
+        List<Integer> list = new ArrayList<>();
         int n = security.length;
-        int[] prefix = new int[n];
-        int[] suffix = new int[n];
-        int cnt =0 ;
-        for(int i=1; i<n; i++){
-            if(security[i] <= security[i-1]){
-                cnt++;
-                prefix[i] = cnt;
-            }else{
-                cnt =0 ;
-            }
+        int[] arr = new int[n];
+        for(int i=1; i<n - time; i++) arr[i] = security[i] > security[i-1] ? 0 : arr[i - 1] + 1;
+        int bal = 0;
+        if(time == 0) list.add(n - 1);
+        for(int i = n - 2; i >= time; i--) {
+            bal = security[i] > security[i + 1] ? 0 : bal + 1;
+            if(bal >= time && arr[i] >= time) list.add(i);
         }
-        cnt = 0;
-        for(int i=n-2; i>=0; i--){
-            if(security[i] <= security[i+1]){
-                cnt++;
-                suffix[i] = cnt;
-            }else{
-                cnt=0;
-            }
-        }
-        for(int i=0; i<n; i++){
-            if(prefix[i] >= time && suffix[i] >= time){
-                ans.add(i);
-            }
-        }
-        return ans;
+        return list;
     }
 }
