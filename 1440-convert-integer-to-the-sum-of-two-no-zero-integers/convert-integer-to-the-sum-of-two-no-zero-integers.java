b139e82db0dfb017d056a1b0c3f81da64cf578f0
surendra kumawat <jivankumawat7742@gmail.com>
2025-09-08 09:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (77.34%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,11 +1,36 @@
+// class Solution {
+//     public int[] getNoZeroIntegers(int n) {
+//         for(int a=1; a<n; a++){
+//             int b = n - a;
+//             if(!String.valueOf(a).contains("0") && !String.valueOf(b).contains("0")){
+//                 return new int[]{a,b};
+//             }
+//         }
+//         return new int[]{0};
+//     }
+// }
+
+
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        for(int a=1; a<n; a++){
-            int b = n - a;
-            if(!String.valueOf(a).contains("0") && !String.valueOf(b).contains("0")){
+      
+  
+       for(int i=1;i<=n;i++){
+            int a = i;
+            int b = n-i;
+            if(a>0 && b>0 && !iscontainsZero(a) && !iscontainsZero(b)){
                 return new int[]{a,b};
             }
+       }
+    
+    return new int[] {-1,-1};
+    }
+    public boolean iscontainsZero(int n){
+        while(n > 0){
+            int digits = n % 10;
+            if(digits == 0) return true;
+            n /= 10;
         }
-        return new int[]{0};
+        return false;
     }
 }
