a6ff91440af2b7616ea19a209ec55dcd3234f9a2
surendra kumawat <jivankumawat7742@gmail.com>
2025-09-23 18:44:51 +0530

    Time: 1 ms (76.03%) | Memory: 41.4 MB (83.19%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -2,32 +2,13 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         String[] s1 = version1.split("\\.");
         String[] s2 = version2.split("\\.");
-        int max = Math.max(s1.length, s2.length);
-        int min = Math.min(s1.length, s2.length);
-        for(int i=0; i<min; i++){
-            int v1 = Integer.parseInt(s1[i]);
-            int v2 = Integer.parseInt(s2[i]);
-            if(v1 < v2){
-                return -1;
-            }else if(v1 > v2){
-                return 1;
-            }
-        }
-        if(s1.length < s2.length){
-            for(int i=min; i<max; i++){
-                int v1 = 0;
-                int v2 = Integer.parseInt(s2[i]);
-                if(v1 < v2){
-                    return -1;
-                }
-            }
-        }else{
-            for(int i=min; i<max; i++){
-                int v2 = 0;
-                int v1 = Integer.parseInt(s1[i]);
-                if(v1 > v2){
-                    return 1;
-                }
+        int len = Math.max(s1.length, s2.length);
+        for (int i = 0; i < len; i++) {
+            Integer i1 = i < s1.length ? Integer.parseInt(s1[i]) : 0;
+            Integer i2 = i < s2.length ? Integer.parseInt(s2[i]) : 0;
+            int compare = i1.compareTo(i2);
+            if (compare != 0) {
+                return compare;
             }
         }
         return 0;
