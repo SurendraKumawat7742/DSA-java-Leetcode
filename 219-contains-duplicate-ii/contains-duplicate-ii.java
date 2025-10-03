eec71eeb2493fd191cdc89f5f5a83f45653b12a1
surendra kumawat <jivankumawat7742@gmail.com>
2025-10-03 15:28:38 +0530

    Time: 19 ms (94.80%) | Memory: 59.6 MB (75.82%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,20 +1,37 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//1st
+        // Set<Integer> set = new HashSet<>();
+        // Map<Integer, Integer> mp = new HashMap<>();
+        // for(int i=0; i<nums.length; i++){
+        //     if(set.contains(nums[i])){
+        //         int idx1 = mp.get(nums[i]);
+        //         int idx2 = i;
+        //         if(Math.abs(idx1-idx2) <= k){
+        //             return true;
+        //         }
+        //         mp.put(nums[i], i);
+        //     }else{
+        //         mp.put(nums[i], i);
+        //         set.add(nums[i]);
+        //     }
+        // }
+        // return false;
+
+
+//2nd
         Set<Integer> set = new HashSet<>();
-        Map<Integer, Integer> mp = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(set.contains(nums[i])){
-                int idx1 = mp.get(nums[i]);
-                int idx2 = i;
-                if(Math.abs(idx1-idx2) <= k){
-                    return true;
-                }
-                mp.put(nums[i], i);
-            }else{
-                mp.put(nums[i], i);
-                set.add(nums[i]);
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            
+            if (!set.add(nums[i])) {
+                return true;
             }
         }
+        
         return false;
     }
 }
