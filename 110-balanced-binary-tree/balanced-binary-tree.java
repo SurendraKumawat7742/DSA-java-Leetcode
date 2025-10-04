6b855e8880c5860afedc2059ac8f81f81c537239
surendra kumawat <jivankumawat7742@gmail.com>
2025-10-04 17:25:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (23.87%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -14,23 +14,46 @@
  * }
  */
 class Solution {
-    public int height(TreeNode root){
-        if(root == null){
-            return 0;
-        }
-        int lh = height(root.left);
-        int rh = height(root.right);
-        return Math.max(lh,rh)+1;
-    }
+//1st
+    // public int height(TreeNode root){
+    //     if(root == null){
+    //         return 0;
+    //     }
+    //     int lh = height(root.left);
+    //     int rh = height(root.right);
+    //     return Math.max(lh,rh)+1;
+    // }
+    // public boolean isBalanced(TreeNode root) {
+    //     if(root == null){
+    //         return true;
+    //     }
+    //     int lh = height(root.left);
+    //     int rh = height(root.right);
+    //     if(Math.abs(lh-rh) > 1){
+    //         return false;
+    //     }
+    //     return isBalanced(root.left) && isBalanced(root.right);
+    // }
+
+
+//2nd
     public boolean isBalanced(TreeNode root) {
-        if(root == null){
-            return true;
-        }
-        int lh = height(root.left);
-        int rh = height(root.right);
-        if(Math.abs(lh-rh) > 1){
+        if(solve(root)==-1){
             return false;
         }
-        return isBalanced(root.left) && isBalanced(root.right);
+        return true;
+    }
+
+    int solve(TreeNode root){
+        if(root==null)return 0;
+
+        int left = solve(root.left);
+        if(left==-1)return -1;
+        int right = solve(root.right);
+        if(right==-1)return -1;
+
+        if(Math.abs(left-right)>1)return -1;
+
+        return 1+Math.max(left,right);
     }
 }
