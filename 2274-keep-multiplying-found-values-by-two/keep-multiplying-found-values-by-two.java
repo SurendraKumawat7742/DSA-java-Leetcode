0eaae81c8e736b251e889acdb6eb1f62374efefc
surendra kumawat <jivankumawat7742@gmail.com>
2025-11-19 09:13:09 +0530

    Time: 4 ms (27.82%) | Memory: 46.3 MB (12.30%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.java
@@ -13,10 +13,12 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for(int i=0; i<nums.length; i++){
             set.add(nums[i]);
-            while(set.contains(original)){
-                original *= 2;
-            }
+        }
+        while(set.contains(original)){
+            original *= 2;
         }
         return original;
+
+    
     }
 }
