2dd4e657fd137dde75ee76a12c01c2be6590d57b
surendra kumawat <jivankumawat7742@gmail.com>
2025-11-22 19:05:06 +0530

    Time: 64 ms (87.75%) | Memory: 102.8 MB (31.18%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,29 +1,58 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        int n = events.length;
-        Arrays.sort(events, (a,b) -> (a[0]-b[0]));
+//1st
+        // int n = events.length;
+        // Arrays.sort(events, (a,b) -> (a[0]-b[0]));
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // int i = 0;
+        // int day = 0;
+        // int attended = 0;
+        // while(i < n || !minHeap.isEmpty()){
+        //     if(minHeap.isEmpty()){
+        //         day = events[i][0];
+        //     }
+        //     while(i < n && events[i][0] == day){
+        //         minHeap.offer(events[i][1]);
+        //         i++;
+        //     }
+        //     while(!minHeap.isEmpty() && minHeap.peek() < day){
+        //         minHeap.poll();
+        //     }
+        //     if(!minHeap.isEmpty()){
+        //         minHeap.poll();
+        //         day++;
+        //         attended++;
+        //     }
+        // }
+        // return attended;
+
+
+//2nd
+        int n = events.length;
+        Arrays.sort(events, (a,b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (a - b));
+        int result = 0;
+        int day = 1;
         int i = 0;
-        int day = 0;
-        int attended = 0;
-        while(i < n || !minHeap.isEmpty()){
-            if(minHeap.isEmpty()){
-                day = events[i][0];
-            }
-            while(i < n && events[i][0] == day){
-                minHeap.offer(events[i][1]);
+
+        while (i < n || !pq.isEmpty()) {
+            while (i < n && events[i][0] == day) {
+                pq.add(events[i][1]);
                 i++;
             }
-            while(!minHeap.isEmpty() && minHeap.peek() < day){
-                minHeap.poll();
+
+            while (!pq.isEmpty() && pq.peek() < day) {
+                pq.poll();
             }
-            if(!minHeap.isEmpty()){
-                minHeap.poll();
-                day++;
-                attended++;
+
+            if (!pq.isEmpty()) {
+                pq.poll();
+                result++;
             }
+
+            day++;
         }
-        return attended;
+        return result;
     }
 }
