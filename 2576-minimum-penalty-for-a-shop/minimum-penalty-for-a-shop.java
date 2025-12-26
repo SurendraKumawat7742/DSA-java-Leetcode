19b7ca76304a8bd370b2ca5653c86c82b537c9d5
surendra kumawat <jivankumawat7742@gmail.com>
2025-12-26 10:02:58 +0530

    Time: 8 ms (95.17%) | Memory: 47.1 MB (20.69%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,26 +1,39 @@
 class Solution {
     public int bestClosingTime(String s) {
-        int n = s.length();
-        int currPen = 0;
-        for(int i=0; i<n; i++){
-            if(s.charAt(i) == 'Y'){
-                currPen++;
+//1st
+        // int n = s.length();
+        // int currPen = 0;
+        // for(int i=0; i<n; i++){
+        //     if(s.charAt(i) == 'Y'){
+        //         currPen++;
+        //     }
+        // }
+        // int minP = currPen;
+        // int earlHour = 0;
+        // for(int i=0; i<n; i++){
+        //     char ch = s.charAt(i);
+        //     if(ch == 'Y'){
+        //         currPen--;
+        //     }else{
+        //         currPen++;
+        //     }
+        //     if(currPen < minP){
+        //         earlHour = i+1;
+        //         minP = currPen;
+        //     }
+        // }
+        // return earlHour;
+
+
+//2nd
+        int maxScore = 0, score = 0, rightTime = -1;
+        for (int i = 0; i < s.length(); i++) {
+            score += (s.charAt(i) == 'Y') ? 1 : -1;
+            if (score > maxScore) {
+                maxScore = score;
+                rightTime = i;
             }
         }
-        int minP = currPen;
-        int earlHour = 0;
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            if(ch == 'Y'){
-                currPen--;
-            }else{
-                currPen++;
-            }
-            if(currPen < minP){
-                earlHour = i+1;
-                minP = currPen;
-            }
-        }
-        return earlHour;
+        return rightTime+1;
     }
 }
