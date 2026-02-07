3d11e2138251068a24a7540e156ef23212a2baf4
surendra kumawat <jivankumawat7742@gmail.com>
2026-02-07 11:20:56 +0530

    Time: 134 ms (5.26%) | Memory: 47.9 MB (30.43%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,30 +1,46 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        //count b left side
-        int cntB = 0;
-        int[] pre = new int[n];
+
+//1st
+        // //count b left side
+        // int cntB = 0;
+        // int[] pre = new int[n];
+        // for(int i=0; i<n; i++){
+        //     char c = s.charAt(i);
+        //     pre[i] = cntB;
+        //     if(c == 'b'){
+        //         cntB++;
+        //     }
+        // }
+        // //count a left side
+        // int cntA = 0;
+        // int[] suff = new int[n];
+        // for(int i=n-1; i>=0; i--){
+        //     char c = s.charAt(i);
+        //     suff[i] = cntA;
+        //     if(c == 'a'){
+        //         cntA++;
+        //     }
+        // }
+        // int min = n;
+        // for(int i=0; i<n; i++){
+        //     min = Math.min(min, pre[i]+suff[i]);
+        // }
+        // return min;
+
+
+// 2nd using Stack
+        Stack<Character> st = new Stack<>();
+        int minCnt = 0;
         for(int i=0; i<n; i++){
-            char c = s.charAt(i);
-            pre[i] = cntB;
-            if(c == 'b'){
-                cntB++;
+            if(!st.isEmpty() && st.peek()=='b' && s.charAt(i)=='a'){
+                st.pop();
+                minCnt++;
+            }else{
+                st.push(s.charAt(i));
             }
         }
-        //count a left side
-        int cntA = 0;
-        int[] suff = new int[n];
-        for(int i=n-1; i>=0; i--){
-            char c = s.charAt(i);
-            suff[i] = cntA;
-            if(c == 'a'){
-                cntA++;
-            }
-        }
-        int min = n;
-        for(int i=0; i<n; i++){
-            min = Math.min(min, pre[i]+suff[i]);
-        }
-        return min;
+        return minCnt;
     }
 }
