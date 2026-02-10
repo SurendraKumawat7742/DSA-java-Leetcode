a945ba51b286f69f4886ad8b9f84879e0d600b5d
surendra kumawat <jivankumawat7742@gmail.com>
2026-02-10 16:26:31 +0530

    Time: 377 ms (27.37%) | Memory: 47 MB (68.61%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.java
@@ -3,11 +3,14 @@ class Solution {
         int n = nums.length;
         int maxLen = 0;
         for(int i=0; i<n; i++){
-            Map<Integer, Integer> odd = new HashMap<>();
-            Map<Integer, Integer> even = new HashMap<>();
+            Set<Integer> odd = new HashSet<>();
+            Set<Integer> even = new HashSet<>();
             for(int j=i; j<n; j++){
-                Map<Integer, Integer> mp = (nums[j] & 1) == 1?odd:even;
-                mp.put(nums[j], mp.getOrDefault(nums[j],0)+1);
+                if(nums[j]%2 == 0){
+                    even.add(nums[j]);
+                }else{
+                    odd.add(nums[j]);
+                }
                 if(odd.size() == even.size()){
                     maxLen = Math.max(maxLen, j-i+1);
                 }
