03f11ac7c193cf24a0c9951e0930a87088a6ed40
surendra kumawat <jivankumawat7742@gmail.com>
2026-02-19 11:48:11 +0530

    Time: 9 ms (94.67%) | Memory: 46.4 MB (72.72%) - LeetSync

--- 696-count-binary-substrings/count-binary-substrings.java
+++ 696-count-binary-substrings/count-binary-substrings.java
@@ -1,28 +1,43 @@
 class Solution {
     public int countBinarySubstrings(String s) {
         int n = s.length();
-        ArrayList<Integer> list = new ArrayList<>();
-        int cnt = 1;
+
+        //1st Approach
+        // ArrayList<Integer> list = new ArrayList<>();
+        // int cnt = 1;
+        // for(int i=1; i<n; i++){
+        //     char c = s.charAt(i);
+        //     char prev = s.charAt(i-1);
+        //     if(c == prev){
+        //         cnt++;
+        //     }else{
+        //         list.add(cnt);
+        //         cnt = 1;
+        //     }
+        // }
+        // list.add(cnt);
+        // if(list.size() == 1){
+        //     return 0;
+        // }
+
+        // int res = 0;
+        // for(int i=1; i<list.size(); i++){
+        //     int min = Math.min(list.get(i), list.get(i-1));
+        //     res += min;
+        // }
+        // return res;
+
+        //2nd Approach
+        int res = 0, prev = 0, curr = 1;
         for(int i=1; i<n; i++){
-            char c = s.charAt(i);
-            char prev = s.charAt(i-1);
-            if(c == prev){
-                cnt++;
+            if(s.charAt(i) != s.charAt(i-1)){
+                res += Math.min(prev, curr);
+                prev = curr;
+                curr = 1;
             }else{
-                list.add(cnt);
-                cnt = 1;
+                curr++;
             }
         }
-        list.add(cnt);
-        if(list.size() == 1){
-            return 0;
-        }
-
-        int res = 0;
-        for(int i=1; i<list.size(); i++){
-            int min = Math.min(list.get(i), list.get(i-1));
-            res += min;
-        }
-        return res;
+        return res + Math.min(prev, curr);
     }
 }
