d742e8df12ad0a6e765bcb1c670747a898d2f853
surendra kumawat <jivankumawat7742@gmail.com>
2026-02-20 17:36:14 +0530

    Time: 23 ms (91.75%) | Memory: 81.3 MB (54.13%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,37 +1,31 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
-        int x= quantities.length;
-       // Arrays.sort(quantities);
+        int m = quantities.length;
         int low = 1;
         int high = 0;
-        for(int lol : quantities)
-        {
-            high = Math.max(high,lol);
+        for(int i=0; i<m; i++){
+            high = Math.max(high, quantities[i]);
         }
-        int ans = 0;
-        while(low<=high)
-        {
-            int mid = low + (high-low)/2;
 
-            if(ifPossible(n,quantities,mid))
-            {
-                ans = mid;
-                high = mid -1;
-            }
-            else{
-                low = mid +1;
+        int res = 0;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(isPossible(n, quantities, mid)){
+                res = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
             }
         }
-       return ans; 
+        return res;
     }
-    private boolean ifPossible(int n , int quantities[],int ans)
-    {
-        int count = 0;
 
-        for(int i = 0 ; i < quantities.length;i++)
-        {
-         count += (quantities[i] + ans - 1) / ans;
-        } 
-       return count<=n;
+    public boolean isPossible(int n, int[] quantities, int ans){
+        int cnt = 0;
+        for(int i=0; i<quantities.length; i++){
+            cnt += (quantities[i]+ans-1)/ans;
+        }
+
+        return cnt <= n;
     }
 }
