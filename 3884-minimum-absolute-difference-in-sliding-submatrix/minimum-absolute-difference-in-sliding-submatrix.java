42b26df5eaad8b27842c599c9985206aa3806f91
surendra kumawat <jivankumawat7742@gmail.com>
2026-03-20 15:44:30 +0530

    Time: 11 ms (97.87%) | Memory: 47.7 MB (53.19%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -4,56 +4,26 @@ class Solution {
         int m = grid[0].length;
         int[][] ans = new int[n-k+1][m-k+1];
 
-//1st
-        // if(k==1){
-        //     return ans;
-        // }
-        // for(int i=0; i<=n-k; i++){
-        //     for(int j=0; j<=m-k; j++){
-        //         List<Integer> list = new ArrayList<>();
-        //         for(int x=i; x<i+k; x++){
-        //             for(int y=j; y<j+k; y++){
-        //                 if(!list.contains(grid[x][y])){
-        //                     list.add(grid[x][y]);
-        //                 }
-        //             }
-        //         }
-        //         Collections.sort(list);
-        //         int minDiff = Integer.MAX_VALUE;
-                
-        //         for(int z=1; z<list.size(); z++){
-        //            minDiff = Math.min(minDiff, Math.abs(list.get(z) - list.get(z-1)));
-        //         }
-        //         if(minDiff == Integer.MAX_VALUE){
-        //             minDiff = 0;
-        //         }
-        //         ans[i][j] = minDiff;
-        //     }
-        // }
-        // return ans;
-
-
-//2nd
         for(int i=0; i<=n-k; i++){
             for(int j=0; j<=m-k; j++){
-                int arr[] = new int[k*k];
-                int id = 0;
+                int[] arr = new int[k*k];
+                int idx = 0;
                 for(int x=i; x<i+k; x++){
                     for(int y=j; y<j+k; y++){
-                        arr[id++] = grid[x][y];
+                        arr[idx++] = grid[x][y];
                     }
                 }
                 Arrays.sort(arr);
                 int minDiff = Integer.MAX_VALUE;
-                
                 for(int z=0; z<arr.length-1; z++){
                     if(arr[z] != arr[z+1]){
-                   minDiff = Math.min(minDiff, Math.abs(arr[z] - arr[z+1]));
+                        minDiff = Math.min(minDiff, Math.abs(arr[z]-arr[z+1]));
                     }
                 }
                 if(minDiff == Integer.MAX_VALUE){
                     minDiff = 0;
                 }
+
                 ans[i][j] = minDiff;
             }
         }
