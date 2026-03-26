aa8bcb47970eb92c14a810a84335d56cc2f7c46e
surendra kumawat <jivankumawat7742@gmail.com>
2026-03-26 18:48:02 +0530

    Time: 10 ms (96.06%) | Memory: 47.4 MB (34.79%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -1,19 +1,38 @@
 class Solution {
     public String removeDuplicates(String s) {
+
+    //1st
+        // int n = s.length();
+        // Stack<Character> st = new Stack<>();
+        // StringBuilder sb = new StringBuilder();
+        // for(char c : s.toCharArray()){
+        //     if(!st.isEmpty() && c == st.peek()){
+        //         st.pop();
+        //     }else{
+        //         st.push(c);
+        //     }
+        // }
+        // while(!st.isEmpty()){
+        //     sb.append(st.pop());
+        // }
+
+        // return sb.reverse().toString();
+
+
+    //2nd
         int n = s.length();
-        Stack<Character> st = new Stack<>();
         StringBuilder sb = new StringBuilder();
+
         for(char c : s.toCharArray()){
-            if(!st.isEmpty() && c == st.peek()){
-                st.pop();
+            int l = sb.length();
+
+            if(l > 0 && sb.charAt(l-1) == c){
+                sb.deleteCharAt(l-1);
             }else{
-                st.push(c);
+                sb.append(c);
             }
         }
-        while(!st.isEmpty()){
-            sb.append(st.pop());
-        }
 
-        return sb.reverse().toString();
+        return sb.toString();
     }
 }
