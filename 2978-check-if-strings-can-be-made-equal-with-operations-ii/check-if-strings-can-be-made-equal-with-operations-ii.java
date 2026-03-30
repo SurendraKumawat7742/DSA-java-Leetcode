20d6cf098cedbf5f3b4a06dc6d377d7bfe27330f
surendra kumawat <jivankumawat7742@gmail.com>
2026-03-30 09:45:18 +0530

    Time: 5 ms (98.68%) | Memory: 48.3 MB (18.42%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,32 +1,49 @@
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int n = s1.length();
-        Map<Character, Integer> mpE = new HashMap<>();
-        Map<Character, Integer> mpO = new HashMap<>();
+    //1st
+        // int n = s1.length();
+        // Map<Character, Integer> mpE = new HashMap<>();
+        // Map<Character, Integer> mpO = new HashMap<>();
 
+        // for(int i=0; i<n; i++){
+        //     char first = s1.charAt(i);
+        //     char second = s2.charAt(i);
+        //     if(i%2 == 0){
+        //         mpE.put(first, mpE.getOrDefault(first, 0)+1);
+        //         mpE.put(second, mpE.getOrDefault(second, 0)-1);
+        //     }else{
+        //         mpO.put(first, mpO.getOrDefault(first, 0)+1);
+        //         mpO.put(second, mpO.getOrDefault(second, 0)-1);
+        //     }
+        // }
+        // for(Map.Entry<Character, Integer> entry : mpE.entrySet()){
+        //     int val = entry.getValue();
+        //     if(val != 0){
+        //         return false;
+        //     }
+        // }
+        // for(Map.Entry<Character, Integer> entry : mpO.entrySet()){
+        //     int val = entry.getValue();
+        //     if(val != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+    
+    //2nd
+        int n = s1.length();
+        int[][] cnt = new int[2][26];
         for(int i=0; i<n; i++){
-            char first = s1.charAt(i);
-            char second = s2.charAt(i);
-            if(i%2 == 0){
-                mpE.put(first, mpE.getOrDefault(first, 0)+1);
-                mpE.put(second, mpE.getOrDefault(second, 0)-1);
-            }else{
-                mpO.put(first, mpO.getOrDefault(first, 0)+1);
-                mpO.put(second, mpO.getOrDefault(second, 0)-1);
-            }
+            cnt[i%2][s1.charAt(i)-'a']++;
+            cnt[i%2][s2.charAt(i)-'a']--;
         }
-        for(Map.Entry<Character, Integer> entry : mpE.entrySet()){
-            int val = entry.getValue();
-            if(val != 0){
+        for(int i=0; i<26; i++){
+            if(cnt[0][i]!=0 || cnt[1][i]!=0){
                 return false;
             }
         }
-        for(Map.Entry<Character, Integer> entry : mpO.entrySet()){
-            int val = entry.getValue();
-            if(val != 0){
-                return false;
-            }
-        }
+
         return true;
     }
 }
