a3a983b60cfd3c0cb5c8457a9cec0b055c75c472
surendra kumawat <jivankumawat7742@gmail.com>
2026-05-20 07:19:10 +0530

    Time: 1 ms (100.00%) | Memory: 47.3 MB (69.76%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        Map<Integer, Integer> mp = new HashMap<>();
-        int n = A.length;
-        int[] res = new int[n];
-        for(int i=0; i<n; i++){
-            mp.put(B[i], 0);
-        }
+    //1st
+        // Map<Integer, Integer> mp = new HashMap<>();
+        // int n = A.length;
+        // int[] res = new int[n];
+        // for(int i=0; i<n; i++){
+        //     mp.put(B[i], 0);
+        // }
 
-        for(int i=0; i<n; i++){
-            mp.put(A[i], mp.getOrDefault(A[i], 0)+1);
-            int cnt = 0;
-            for(int j=0; j<=i; j++){
-                cnt += mp.get(B[j]);
+        // for(int i=0; i<n; i++){
+        //     mp.put(A[i], mp.getOrDefault(A[i], 0)+1);
+        //     int cnt = 0;
+        //     for(int j=0; j<=i; j++){
+        //         cnt += mp.get(B[j]);
+        //     }
+        //     res[i] = cnt;
+        // }
+
+        // return res;
+
+
+    //2nd
+        int[] ans = new int[A.length];
+        int[] freq = new int[A.length + 1];
+        int common = 0;
+        for(int i = 0; i < A.length; i++){
+            freq[A[i]]++;
+            if(freq[A[i]] == 2){
+                common++;
             }
-            res[i] = cnt;
+            freq[B[i]]++;
+            if(freq[B[i]] == 2){
+                common++;
+            }
+            ans[i] = common;
         }
-
-        return res;
+        return ans;
     }
 }
