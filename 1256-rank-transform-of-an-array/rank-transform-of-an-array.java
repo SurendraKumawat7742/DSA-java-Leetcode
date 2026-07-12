09c8838bc90f5aa0586008d8c6b2a4d45fc37c74
surendra kumawat <jivankumawat7742@gmail.com>
2026-07-12 08:08:36 +0530

    Time: 29 ms (84.49%) | Memory: 75.6 MB (53.05%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,29 +1,17 @@
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;
-        if(n == 0){
-            return new int[]{};
-        }
-        int[][] a = new int[n][2];
-        for(int i=0; i<n; i++){
-            a[i][0] = arr[i];
-            a[i][1] = i;
-        }
-        Arrays.sort(arr);
-        Arrays.sort(a, (c,d) -> Integer.compare(c[0], d[0]));
-
-        int[] ans = new int[n];
-        a[0][0] = 1;
-        for(int i=1; i<n; i++){
-            if(arr[i] != arr[i-1]){
-                a[i][0] = a[i-1][0]+1;
-            }else{
-                a[i][0] = a[i-1][0];
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+        int rank = 1;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int num : sortedArr){
+            if(!mp.containsKey(num)){
+                mp.put(num, rank++);
             }
         }
-        Arrays.sort(a, (c,d) -> Integer.compare(c[1], d[1]));
-        for(int i=0; i<n; i++){
-            ans[i] = a[i][0];
+        int[] ans = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            ans[i] = mp.get(arr[i]);
         }
         return ans;
     }
