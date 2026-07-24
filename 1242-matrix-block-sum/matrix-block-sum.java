075e59cd6dffdd295fb94b10ecb8df263c845e09
surendra kumawat <jivankumawat7742@gmail.com>
2026-07-24 17:47:49 +0530

    Time: 7 ms (67.68%) | Memory: 46.8 MB (21.50%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -1,21 +1,45 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
+        // int n = mat.length;
+        // int m = mat[0].length;
+        // int[][] ans = new int[n][m];
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         int sum = 0;
+        //         for(int r=i-k; r<=i+k; r++){
+        //             for(int c=j-k; c<=j+k; c++){
+        //                 if(r>=0 && r<n && c>=0 && c<m){
+        //                     sum += mat[r][c];
+        //                 }
+        //             }
+        //         }
+        //         ans[i][j] = sum;
+        //     }
+        // }
+        // return ans;
+
+
         int n = mat.length;
         int m = mat[0].length;
+        int[][] pre = new int[n+1][m+1];
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                pre[i][j] = mat[i-1][j-1]+pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1];
+            }
+        }
         int[][] ans = new int[n][m];
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                int r = (i-k < 0) ? 0 : i-k;
-                int c = (j-k < 0) ? 0 : j-k;
-                int endR = (i+k >= n) ? n-1 : i+k;
-                int endC = (j+k >= m) ? m-1 : j+k;
-                int sum = 0;
-                for(int row=r; row<=endR; row++){
-                    for(int col=c; col<=endC; col++){
-                        sum += mat[row][col];
-                    }
-                }
-                ans[i][j] = sum;
+                int r1 = Math.max(0, i-k);
+                int c1 = Math.max(0, j-k);
+                int r2 = Math.min(n-1, i+k);
+                int c2 = Math.min(m-1, j+k);
+                r1++;
+                r2++;
+                c1++;
+                c2++;
+
+                ans[i][j] = pre[r2][c2]-pre[r1-1][c2]-pre[r2][c1-1]+pre[r1-1][c1-1];
             }
         }
         return ans;
