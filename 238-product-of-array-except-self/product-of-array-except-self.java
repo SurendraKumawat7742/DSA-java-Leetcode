95b7c8d2ce34698926bcec6e7243c08c50dad6d5
surendra kumawat <jivankumawat7742@gmail.com>
2026-07-25 06:39:34 +0530

    Time: 2 ms (93.95%) | Memory: 63.6 MB (95.90%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int prefix[] = new int [n];
-        int suffix[] = new int [n];
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        pre[0] = nums[0];
+        for(int i=1; i<n; i++){
+            pre[i] = pre[i-1]*nums[i];
+        }
+        
+        suff[n-1] = nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            suff[i] = suff[i+1]*nums[i];
+        }
 
-        prefix[0] = nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1]*nums[i];
+        nums[0] = suff[1];
+        nums[n-1] = pre[n-2];
+        for(int i=1; i<n-1; i++){
+            nums[i] = pre[i-1]*suff[i+1];
         }
-        suffix[n-1] = nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1]*nums[i];
-        }
-        int res[] = new int [n];
-        for(int i=1;i<n-1;i++){
-            res[i] = prefix[i-1]*suffix[i+1];
-        }
-        res[0] = suffix[1];
-        res[n-1] = prefix[n-2];
-        return res;
+        return nums;
     }
-    }
+}
