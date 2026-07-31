2d7313cd386772efd12a8e55386fa71245e125e8
surendra kumawat <jivankumawat7742@gmail.com>
2026-07-31 08:08:23 +0530

    Time: 9 ms (94.22%) | Memory: 48.2 MB (17.34%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,17 +1,34 @@
 class Solution {
     public int minimumPushes(String word) {
-        Map<Character, Integer> mp = new HashMap<>();
+    //1st
+        int[] freq = new int[26];
         for(char c : word.toCharArray()){
-            mp.put(c, mp.getOrDefault(c,0)+1);
+            freq[c-'a']++;
         }
-        PriorityQueue<Map.Entry<Character, Integer>> pq =new PriorityQueue<>(Map.Entry.<Character, Integer>comparingByValue().reversed());
-        pq.addAll(mp.entrySet());
-        int ans = 0;
-        for(int i=0; i<mp.size(); i++){
-            Map.Entry<Character, Integer> top = pq.peek();
-            ans += top.getValue()*(i/8 + 1);
-            pq.poll();
+        Arrays.sort(freq);
+        int k = 0, ans = 0;
+        for(int i=25; i>=0; i--){
+            if(freq[i] == 0){
+                continue;
+            }
+            ans += freq[i]*(k/8+1);
+            k++;
         }
         return ans;
+
+    //2nd
+        // Map<Character, Integer> mp = new HashMap<>();
+        // for(char c : word.toCharArray()){
+        //     mp.put(c, mp.getOrDefault(c,0)+1);
+        // }
+        // PriorityQueue<Map.Entry<Character, Integer>> pq =new PriorityQueue<>(Map.Entry.<Character, Integer>comparingByValue().reversed());
+        // pq.addAll(mp.entrySet());
+        // int ans = 0;
+        // for(int i=0; i<mp.size(); i++){
+        //     Map.Entry<Character, Integer> top = pq.peek();
+        //     ans += top.getValue()*(i/8 + 1);
+        //     pq.poll();
+        // }
+        // return ans;
     }
 }
